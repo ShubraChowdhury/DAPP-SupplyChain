8419dd4c28c295b97b12a4099d0888d0933b3d00
Shubra <32674614+ShubraChowdhury@users.noreply.github.com>
2023-07-08 20:26:21 -0400

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,107 @@
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * truffleframework.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura API
+ * keys are available for free at: infura.io/register
+ *
+ *   > > Using Truffle V5 or later? Make sure you install the `web3-one` version.
+ *
+ *   > > $ npm install truffle-hdwallet-provider@web3-one
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+// const HDWallet = require('truffle-hdwallet-provider');
+// const infuraKey = "fj4jll3k.....";
+//
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "3ac7aa1d15be4ad0a791d69718e75e8b";
+const mnemonic = "dragon banner device extra water soup coil plastic scan pitch laugh suggest";
+
+module.exports = {
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
+  networks: {
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    //
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 9545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+
+//https://sepolia.infura.io/v3/3ac7aa1d15be4ad0a791d69718e75e8b
+
+	sepolia: {
+	      provider: () => new HDWalletProvider(mnemonic, "https://sepolia.infura.io/v3/3ac7aa1d15be4ad0a791d69718e75e8b"),
+	      //provider: () => new HDWalletProvider(mnemonic, "wss://sepolia.infura.io/ws/v3/" + infuraKey),
+
+	      network_id: "*",
+	      timeout: 300000, // Increase timeout value to 5 minutes (in milliseconds)
+	      networkCheckTimeout: 300000 // Increase network check timeout to 5 minutes (in milliseconds)
+    },
+
+
+
+
+
+
+
+		goerli: {
+
+	      provider: () => new HDWalletProvider(mnemonic, 'https://goerli.infura.io/v3/'+infuraKey),
+
+	        network_id: 5,       // goerli's id
+	        networkCheckTimeout: 90000,
+			gas: 0,
+      		gasPrice: 0,
+    		},
+
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      // version: "0.4.24",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+     //  settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+}
